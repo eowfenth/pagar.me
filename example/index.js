70e80fba5083ef2f539595c51c7cb1a7d2ad7032
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-06 11:17:19 -0300

    refactor: example webserver to follow DRY

--- example/index.js
+++ example/index.js
@@ -1,32 +1,7 @@
 const http = require("http");
-const { capture , charge, refund } = require('../index');
+const app  = require("./src/app");
+const { config: { PORT }} = require("./src/config");
 
-const server = http.createServer(async (req, res) => {
-    if (req.method === "POST") {
-        let body = "";
-        
-        req.on('data', (chunk) => {
-            body += chunk;
-        });
+const server = http.createServer(app);
 
-        req.on('end', async () => {
-            const request = JSON.parse(body);
-
-            if (req.url === "/charge") {
-                const card = request.card;
-                const value = request.value;
-                const response = await charge(card, value, true);
-
-                res.write(JSON.stringify(response));
-                res.end();
-                return;
-            }
-    
-            if (req.url === "/capture") {}
-    
-            if (req.url === "refund") {}
-        });
-    }
-});
-
-server.listen(8081);
+server.listen(PORT);
