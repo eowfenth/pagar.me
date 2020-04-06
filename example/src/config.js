e85d4b121cdc478d5eb79afa3c1b3b69678d6a89
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-06 11:15:57 -0300

    feat: add config file for example webserver

---
+++ example/src/config.js
@@ -0,0 +1,10 @@
+exports.config = {
+    PORT: process.env.PORT || 8081,
+};
+
+exports.defaultHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': 2592000,
+};
